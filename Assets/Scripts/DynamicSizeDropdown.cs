337eb2ed234302c19565f7e6a56e2c5e2159bffa
Sam beard <sam210802@gmail.com>
2023-02-13 18:32:47 +0000

    Added script to DynamicDropdown Script ensures dropdown has correct option displayed on awake

---
+++ Assets/Scripts/DynamicSizeDropdown.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class DynamicSizeDropdown : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Dropdown dropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var listOfOptions = dropdown.options.Select(OptionsMenu => OptionsMenu.text).ToList();
+        dropdown.value = listOfOptions.IndexOf(OptionsMenu.loadTextPrefs());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
